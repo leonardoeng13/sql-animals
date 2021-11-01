630fc533363b55d8321dd3ef7add308fe2a11409
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-11-01 14:38:59 -0300

    Schema and querys

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals values (default, 'Agumon', '20200203', 0, true, 10.23, CURRENT_USER, now()); 
+insert into animals values (default, 'Gabumon', '20181115', 2, true, 8.00, CURRENT_USER, now());
+insert into animals values (default, 'Pikachu', '20210107', 1, false, 15.04, CURRENT_USER, now());
+insert into animals values (default, 'Devimon', '20170512', 5, true, 11.00, CURRENT_USER, now());
+
--- schema.sql
+++ schema.sql
@@ -1,5 +1,17 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+create database vet_clinic;
+
+create table animals (
+id serial,
+name varchar(45),
+date_of_birth date,
+escape_attempts integer,
+neutered boolean,
+weight_kg decimal(10,4),
+user_name varchar (45),
+timestamp timestamp,
+primary key (id)
 );
+
+
